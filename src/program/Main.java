a3e0dc77985549979dde6f5d5743c326029d3cfe
Levelvini <viniciusfc29@gmail.com>
2024-07-22 10:02:18 -0300

    BREAKING CHANGE:  project structure change

--- src/program/Main.java
+++ src/program/Main.java
@@ -3,21 +3,38 @@ package program;
 public class Main {
     public static void main(String[] args) {
 
+        int[] a = {2,4,6,1,10,7,9,3,12,21,7};
+        int[] b = new int[25];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = (int) Math.floor(Math.random() * b.length);
+        }
         //Algoritmo bubble sort
 
-        int[] v = {2,4,6,1,10};
-        for (int i = 0; i < v.length; i++) {
-            for (int j = 0; j < v.length - i -1; j++) {
-            if (v[j] > v[j+1]) {
-                int aux = v[j];
-                v[j] = v[j+1];
-                v[j+1] = aux;
-            }
-            }
-        }
+        bubbleSort(a);
+        bubbleSort(b);
         System.out.print("bubble sort: ");
-        for (int i = 0; i < v.length; i++) {
-            System.out.print(v[i]+", ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+", ");
+        }
+        System.out.println();
+        System.out.print("bubbleSort: ");
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i]+", ");
         }
     }
+    private static int[] bubbleSort(int[] v) {
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v.length - i -1; j++) {
+                if (v[j] > v[j+1]) {
+                    int aux = v[j];
+                    v[j] = v[j+1];
+                    v[j+1] = aux;
+                }
+            }
+        }return v;
+    }
+   // --------------------------------------------------------------------------
+
+    //algoritmo quicksort
+
 }
